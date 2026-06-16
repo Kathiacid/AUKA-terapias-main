8b04d2708a104ff13810414e9ae5e2184560c941
kat <kathiacid98@gmail.com>
2026-06-15 21:23:30 -0400

    f

--- AUKA-terapias-main/backend/auka-main/auka/auka/wsgi.py
+++ AUKA-terapias-main/backend/auka-main/auka/auka/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# Esto le dice a Python que busque las carpetas internas de forma automática
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.dirname(BASE_DIR))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auka.settings')
